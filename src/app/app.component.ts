1285be13ef827d464328a96e23007667c0df3741
Mikita Subbot <mikita_subbot@epam.com>
2017-12-18 15:30:47 +0300

    feat(record): added speech record simulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,75 +1,75 @@
 import { Component } from "@angular/core";
 
 type Speach = {
-  username: string;
-  selectedSpeachType: string;
-  speachResult: string;
-  date: string;
+    username: string;
+    selectedSpeachType: string;
+    speachResult: string;
+    date: string;
 };
 
 @Component({
-  selector: "app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+    selector: "app",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "app";
-  speachTypes = ["public", "camarades", "official", "relax"];
-  speachResults = ["good", "not good", "bad", "bad like your mama"];
-  options = {
-    era: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    weekday: "long",
-    timezone: "UTC",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-  };
-  selectedSpeachType: string;
-  username: string;
-  speachResult: string;
-  speaches: Speach[] = [
-    {
-      username: "Harrison Ford",
-      selectedSpeachType: this.speachTypes[0],
-      speachResult: this.speachResults[0],
-      date: new Date().toLocaleString("en-US", this.options)
-    },
-    {
-      username: "P.Poroshenko",
-      selectedSpeachType: "hohol",
-      speachResult: "slava for the ukraine",
-      date: new Date().toLocaleString("en-US", this.options)
-    },
-  ];
+    title = "app";
+    speachTypes = ["public", "camarades", "official", "relax"];
+    speachResults = ["good", "not good", "bad", "not bad", "awesome"];
+    options = {
+        era: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        weekday: "long",
+        timezone: "UTC",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    };
+    selectedSpeachType: string;
+    username: string;
+    speachResult: string;
+    speaches: Speach[] = [
+        {
+            username: "Harrison Ford",
+            selectedSpeachType: this.speachTypes[0],
+            speachResult: this.speachResults[0],
+            date: new Date().toLocaleString("en-US", this.options)
+        },
+        {
+            username: "P.Poroshenko",
+            selectedSpeachType: "hohol",
+            speachResult: "slava for the ukraine",
+            date: new Date().toLocaleString("en-US", this.options)
+        },
+    ];
 
-  articles = [
-    {
-      title: "top way to get top speaker only for 0.99$",
-      text: "im joking, it cost 100$",
-      picture: "../assets/pics/pic.jpg",
-      description: "some butiful article"
-    },
-    {
-      title: "A.Damboldor: Lie or Die",
-      text: "im joking, it cost 10000$",
-      picture: "../assets/pics/pic.jpg",
-      description: "some butiful article"
-    },
-    {
-      title: "3 ways to speak like D.Trump",
-      text: "im joking, it cost 1000$",
-      picture: "../assets/pics/pic.jpg",
-      description: "some butiful article"
-    },
-    {
-      title: "Nothing is free, exept your words",
-      text: "im joking, it cost 10000$",
-      picture: "../assets/pics/pic.jpg",
-      description: "some butiful article"
-    }
-  ];
+    articles = [
+        {
+            title: "top way to get top speaker only for 0.99$",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque.",
+            picture: "../assets/pics/pic.jpg",
+            description: "some butiful article"
+        },
+        {
+            title: "A.Damboldor: Lie or Die",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque.",
+            picture: "../assets/pics/pic.jpg",
+            description: "some butiful article"
+        },
+        {
+            title: "3 ways to speak like D.Trump",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque.",
+            picture: "../assets/pics/pic.jpg",
+            description: "some butiful article"
+        },
+        {
+            title: "Nothing is free, exept your words",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id nulla erat. Etiam nec nisi quis risus viverra tristique non et ipsum. Duis molestie lobortis nibh, a varius ex semper nec. Maecenas sed maximus ipsum. Curabitur et magna a tortor mollis placerat. Aenean ornare imperdiet mauris vitae dictum. In hac habitasse platea dictumst. Proin eget cursus erat. Proin posuere ipsum at ante consequat, quis sollicitudin mi condimentum. Nam vehicula massa at dolor fermentum, a malesuada sem porttitor. Donec congue volutpat augue ac rhoncus. Pellentesque mattis tincidunt pellentesque.",
+            picture: "../assets/pics/pic.jpg",
+            description: "some butiful article"
+        }
+    ];
 
 }
--- src/app/speach/speach.component.ts
+++ src/app/speach/speach.component.ts
@@ -1,54 +1,68 @@
 import { Component, Input } from "@angular/core";
 
 type Speach = {
-  username: string;
-  selectedSpeachType: string;
-  speachResult: string;
-  date: string;
+    username: string;
+    selectedSpeachType: string;
+    speachResult: string;
+    date: string;
+    record: boolean;
 };
 
 @Component({
-  selector: "sr-speach",
-  templateUrl: "./speach.component.html",
-  styleUrls: ["./speach.component.css"]
+    selector: "sr-speach",
+    templateUrl: "./speach.component.html",
+    styleUrls: ["./speach.component.css"]
 })
 export class SpeachComponent {
-  @Input() username: string;
-  @Input() selectedSpeachType: string;
-  @Input() speachResult: string;
-  @Input() speachResults: string[];
-  @Input() speachTypes: string[];
-  @Input() speaches: Speach[];
+    @Input() username: string;
+    @Input() selectedSpeachType: string;
+    @Input() speachResult: string;
+    @Input() speachResults: string[];
+    @Input() speachTypes: string[];
+    @Input() speaches: Speach[];
 
-  isValidSpeach() {
-    return (
-      this.username !== undefined &&
-      this.selectedSpeachType !== undefined &&
-      this.speachResult !== undefined
-    );
-  }
+    isRecording: boolean = false;
+    recordExist: boolean = false;
 
-  addSpeach() {
-    if (this.isValidSpeach()) {
-      const newSpeach: Speach = {
-        username: this.username,
-        selectedSpeachType: this.selectedSpeachType,
-        speachResult: this.speachResult,
-        date: new Date().toLocaleString("en-US", this.options)
-      };
-      this.speaches.push(newSpeach);
+    isValidSpeach() {
+        return (
+            this.username !== undefined &&
+            this.selectedSpeachType !== undefined &&
+            this.speachResult !== undefined && this.recordExist
+        );
     }
-  }
 
-  options = {
-    era: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    weekday: "long",
-    timezone: "UTC",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-  };
+    recordSpeech() {
+        this.isRecording = true;
+    }
+
+    endRecording() {
+        this.isRecording = false;
+        this.recordExist = true;
+    }
+
+    addSpeach() {
+        if (this.isValidSpeach()) {
+            const newSpeach: Speach = {
+                username: this.username,
+                selectedSpeachType: this.selectedSpeachType,
+                speachResult: this.speachResult,
+                date: new Date().toLocaleString("en-US", this.timeOptions),
+                record: true,
+            };
+            this.speaches.push(newSpeach);
+        }
+    }
+
+    timeOptions = {
+        era: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        weekday: "long",
+        timezone: "UTC",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    };
 }
