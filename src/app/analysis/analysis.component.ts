f3e6f82760569b294964c9af5fc19d8b3c633522
Nikita Subbot <nsubbot17@gmail.com>
2017-12-27 05:04:31 +0300

    feat(analyse): added support of showing analyse

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,8 +1,23 @@
 import { Component, Input } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { SWAGGER_API_HOST } from "../config";
+import { Analyse } from "../dto/Analyse";
 @Component({
     selector: "sr-analysis",
     templateUrl: "./analysis.component.html",
     styleUrls: ["./analysis.component.css"]
 })
 export class AnalysisComponent {
+    analyse;
+
+    constructor(private httpClient: HttpClient) {
+        this.httpClient
+            .get(SWAGGER_API_HOST + "/analysis")
+            .toPromise()
+            .then(response => {
+                this.analyse = response as Analyse;
+            })
+            .catch(console.log);
+    }
+
 }
---
+++ src/app/dto/Analyse.ts
@@ -0,0 +1,16 @@
+export type Analyse = {
+    PercentOfSpeechTypeAccordance: number,
+    PercentOfWordsParasites: number,
+    WordsParasites: number,
+    PercentOfWordsRepetitions: number,
+    WordsRepetitions: number,
+    PercentOfSpeechPlaceAccordance: number,
+    SpeedSpechAccordance: number,
+    AudibilityAccordance: number,
+    PercentOfFacialExpressionAccordance: number,
+    PercentOfGesturesAccordance: number,
+    PercentOfArticulationAccordance: number,
+    PercentOfFormAccordance: number,
+    Tips: null | any,
+    Articles: null | any,
+};
